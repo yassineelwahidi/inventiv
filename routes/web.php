a42ddc0b3075f62e3121eb511b314417b952f3ca
Yassine Elwhahidi <yassine.elwahidi@gmail.com>
2019-12-11 14:12:25 +0100

    add middleware auth

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', 'PostController@index');
-Route::post('/post/add', 'PostController@store');
-Route::delete('/post/delete/{post}', 'PostController@destroy');
-Route::post('/post/publish/{id}', 'PostController@publish');
-Route::get('/post/edit/{id}', 'PostController@edit');
-Route::put('/post/update', 'PostController@update');
-Route::get('/categories', 'CategoryController@index');
+Route::group(['middleware' => ['auth']],function(){
+    Route::get('/posts', 'PostController@index');
+    Route::post('/post/add', 'PostController@store');
+    Route::delete('/post/delete/{post}', 'PostController@destroy');
+    Route::post('/post/publish/{id}', 'PostController@publish');
+    Route::get('/post/edit/{id}', 'PostController@edit');
+    Route::put('/post/update', 'PostController@update');
+    Route::get('/categories', 'CategoryController@index');
 
-Route::get('/offers', 'OfferController@index');
-Route::post('/offer/add', 'OfferController@store');
-Route::delete('/offer/delete/{offer}', 'OfferController@destroy');
-Route::post('/offer/publish/{id}', 'OfferController@publish');
-Route::get('/offer/edit/{id}', 'OfferController@edit');
-Route::put('/offer/update', 'OfferController@update');
-Route::get('/activities', 'ActivityController@index');
+    Route::get('/offers', 'OfferController@index');
+    Route::post('/offer/add', 'OfferController@store');
+    Route::delete('/offer/delete/{offer}', 'OfferController@destroy');
+    Route::post('/offer/publish/{id}', 'OfferController@publish');
+    Route::get('/offer/edit/{id}', 'OfferController@edit');
+    Route::put('/offer/update', 'OfferController@update');
+    Route::get('/activities', 'ActivityController@index');
+});
+
 
 Route::post('/users/register', 'UserController@register')->middleware('guest');
 Route::post('/users/login', 'UserController@login')->middleware('guest');
