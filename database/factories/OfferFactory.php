1d27c72719e6dcd91784f65eff4a209351cfc4e8
Yassine Elwhahidi <yassine.elwahidi@gmail.com>
2019-12-11 14:40:24 +0100

    some updates

--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -6,12 +6,13 @@ use App\Offer;
 use Faker\Generator as Faker;
 
 $factory->define(Offer::class, function (Faker $faker) {
+    $bl = $faker->boolean;
     return [
         'title'=>$faker->sentence(rand(5,10)),
         'text'=>$faker->paragraph(rand(200,500)),
         'created_by'=>App\User::all()->random()->id,
-        'is_published'=>$faker->boolean,
-        'published_by'=>App\User::all()->random()->id,
+        'is_published'=>$bl,
+        'published_by'=>$bl == true ? App\User::all()->random()->id : null,
         'activity_id'=>App\Activity::all()->random()->id,
     ];
 });
